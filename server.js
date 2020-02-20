4bf2d151049a70eaeb5b2845597fb8a3b2ba35de
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-20 17:26:10 -0500

    updated the .get to get id items

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const express = require("cartItems");
 const cartItems = express.Router();
+const cors = require('cors');
 
 // creates an instance of an Express server this is referencing the const express
 const app = express();
@@ -13,7 +14,6 @@ const port = 8080;
 app.use (express.json());
 
 // request 
-
 app.use('/', __dirname,cartItems);
 
 
@@ -22,7 +22,7 @@ app.use('/', __dirname,cartItems);
 // get json array of all items in the cart-items
 //  status code 200
 
-cartItems.get ('/cart-items', function (req,res){
+cartItems.get ('/cart-items', cors(corsOptions), function (req,res){
     // req is declared but never used error message, need to find out what this means 
     // https://flaviocopes.com/express-request-parameters/
     console.log(reg.body);
@@ -33,16 +33,16 @@ cartItems.get ('/cart-items', function (req,res){
 
 // get json response of :id
 // status code 200 
-cartItems.get('/cartItems.js:id', function (req,res){
+cartItems.get('/cartItems.js:id', cors(corsOptions),function (req,res){
     console.log(reg.body); //data being extracted from the URL
-    return res.json(cart-items,id);
+    return res.json(cart-items,id[req.params.index]); // need to get stuff from the array 
     res.send("get ID for item in the cart.." + req.params.id);
     res.status(200); //ok
 });
 
 // add a cart item to the array using the json body of the request , generate a unique id for that ID
 // status code 201 
-cartItems.post('/cartItems.js/:id', function (req,res){
+cartItems.post('/cartItems.js/:id', cors(corsOptions), function (req,res){
     // missing something here ?
     console.log(reg.body); //data being extracted from the URL      
 res.send("Add items to the cart ..")
@@ -53,8 +53,8 @@ res.status(201); // created
 // update the cart in the that has a unique id, use JSON body as the new porperty
 //  response 200
 
-cartItems.put(" '/cartItems.js/:id", function (req,res){
-// missing something here ?
+cartItems.put(" '/cartItems.js/:id", cors(corsOptions),function (req,res){
+// missing something here ? need push to add item from the array 
     console.log(req.params.id);  //URL paramaters
     res.send("Update items in the cart ..")
     res.status(200); //ok
