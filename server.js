1d626a3d565dae8598f990a00abcc7f1bdf87813
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-20 18:55:33 -0500

    added cors as a function,

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cartItems = require("./cartItems");
 const cors = require('cors');
 
+
 // creates an instance of an Express server this is referencing the const express
 const app = express();
 
@@ -11,9 +12,10 @@ const port = 8080;
 
 //convert to json
 app.use (express.json());
+app.use (cors());
 
 // request 
-app.use('/', __dirname,cartItems);
+// app.use('/', __dirname,cartItems);
 
 
 // Endpoints 
@@ -21,7 +23,7 @@ app.use('/', __dirname,cartItems);
 // get json array of all items in the cart-items
 //  status code 200
 
-app.get ('/cartItems', cors(corsOptions), function (req,res){
+app.get ('/cartItems',  function (req,res){
     // req is declared but never used error message, need to find out what this means 
     // https://flaviocopes.com/express-request-parameters/
     res.status(200);//ok
@@ -33,7 +35,7 @@ app.get ('/cartItems', cors(corsOptions), function (req,res){
 
 // get json response of :id
 // status code 200 
-app.get('/cartItems/:id', cors(corsOptions),function (req,res){
+app.get('/cartItems/:id',function (req,res){
     console.log(reg.body); //data being extracted from the URL
     res.status(200); //ok
     return res.json(cart-items,id[req.params.index]); // need to get stuff from the array 
@@ -43,7 +45,7 @@ app.get('/cartItems/:id', cors(corsOptions),function (req,res){
 
 // add a cart item to the array using the json body of the request , generate a unique id for that ID
 // status code 201 
-app.post('/cartItems/:id', cors(corsOptions), function (req,res){
+app.post('/cartItems/:id', function (req,res){
     // missing something here ? need to find out what you use to add to the array
     res.status(201); // created
     console.log(reg.body); //data being extracted from the URL      
@@ -55,7 +57,7 @@ res.send("Add items to the cart ..")
 // update the cart in the that has a unique id, use JSON body as the new porperty
 //  response 200
 
-app.put("/cartItems/:id", cors(corsOptions),function (req,res){
+app.put("/cartItems/:id", function (req,res){
 // missing something here ? need push to add item from the array
 res.status(200); //ok 
     console.log(req.params.id);  //URL paramaters
@@ -66,7 +68,7 @@ res.status(200); //ok
 
 // remove the item from the array that has the given id 
 // response status 204
-app.delete('/cartItems/:id', cors(corsOptions), function (req,res){
+app.delete('/cartItems/:id',  function (req,res){
     // need to do a splice here to remove items from the array, I think 
     res.status(204) //no content
     cartItems.splice(index, 1); //remove an item from the cart, not sure about the -1
