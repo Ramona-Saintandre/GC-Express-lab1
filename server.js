d25a12586344b925105bdd8700a27039aeb25ffd
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-20 18:33:19 -0500

    changed cartItems to app

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/', __dirname,cartItems);
 // get json array of all items in the cart-items
 //  status code 200
 
-cartItems.get ('/cart-items', cors(corsOptions), function (req,res){
+app.get ('/cart-items', cors(corsOptions), function (req,res){
     // req is declared but never used error message, need to find out what this means 
     // https://flaviocopes.com/express-request-parameters/
     console.log(reg.body);
@@ -33,7 +33,7 @@ cartItems.get ('/cart-items', cors(corsOptions), function (req,res){
 
 // get json response of :id
 // status code 200 
-cartItems.get('/cartItems.js:id', cors(corsOptions),function (req,res){
+app.get('/cartItems.js:id', cors(corsOptions),function (req,res){
     console.log(reg.body); //data being extracted from the URL
     return res.json(cart-items,id[req.params.index]); // need to get stuff from the array 
     res.send("get ID for item in the cart.." + req.params.id);
@@ -42,7 +42,7 @@ cartItems.get('/cartItems.js:id', cors(corsOptions),function (req,res){
 
 // add a cart item to the array using the json body of the request , generate a unique id for that ID
 // status code 201 
-cartItems.post('/cartItems.js/:id', cors(corsOptions), function (req,res){
+app.post('/cartItems.js/:id', cors(corsOptions), function (req,res){
     // missing something here ? need to find out what you use to add to the array
     console.log(reg.body); //data being extracted from the URL      
 res.send("Add items to the cart ..")
@@ -53,7 +53,7 @@ res.status(201); // created
 // update the cart in the that has a unique id, use JSON body as the new porperty
 //  response 200
 
-cartItems.put(" '/cartItems.js/:id", cors(corsOptions),function (req,res){
+app.put(" '/cartItems.js/:id", cors(corsOptions),function (req,res){
 // missing something here ? need push to add item from the array 
     console.log(req.params.id);  //URL paramaters
     res.send("Update items in the cart ..")
@@ -63,7 +63,7 @@ cartItems.put(" '/cartItems.js/:id", cors(corsOptions),function (req,res){
 
 // remove the item from the array that has the given id 
 // response status 204
-cartItems.delete('/cartItems.js/:id', cors(corsOptions), function (req,res){
+app.delete('/cartItems.js/:id', cors(corsOptions), function (req,res){
     // need to do a splice here to remove items from the array, I think 
     cartItems.splice(index, -1); //remove an item from the cart, not sure about the -1
    console.log(req.params.id);  //URL parameters 
@@ -74,6 +74,6 @@ cartItems.delete('/cartItems.js/:id', cors(corsOptions), function (req,res){
 // update the cart item in the array that has the given id , use the JSON body 
 // run the server
 
-cartItems.listen(port, () => console.log(`Listening on port: ${port}.`));
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
 console.log('Listening at http://localhost:8080/cart-items...');
 
