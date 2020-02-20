c4bf79213c5b697dc79337363984742d37ee24fa
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-19 22:10:18 -0500

    added module export to cart-items.js

--- cart-items.js
+++ cart-items.js
@@ -1,33 +1,33 @@
 const cartList = [
     {
         id:1,
-    product::"milk",
+    product:"milk",
     price:3.29,
     quantity:2,
 },
 {
 id:1,
-    product::"milk",
+    product:"milk",
     price:3.29,
     quantity:2,
 },
 {
 id:1,
-    product::"bread",
+    product:"bread",
     price:1.29,
     quantity:5,
 },
 {
 id:1,
-    product::"eggs",
+    product:"eggs",
     price:1.79,
     quantity:3,
 },
 {
 id:1,
-    product::"toliet paper",
+    product:"toliet paper",
     price:17.99,
     quantity:2,
-}]
+}];
 
-module.exports = { "cartItems "};
+module.exports = { " cartItems "}
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 // require the Express module
 const express = require("express");
-// creates an instance of an Express server
+const express = require("cart-items");
+
+// creates an instance of an Express server this is referencing the const express
 const app = express();
+
 // define the port
-const port = 3000;
+const port = 8080;
+
+//convert to json
+app.use (express.json());
+
+// request 
+
+app.use('/', __dirname,cart-items);
+
 // run the server
 app.listen(port, () => console.log(`Listening on port: ${port}.`));
+console.log('Listening at http://localhost:8080/cart-items...');
+
